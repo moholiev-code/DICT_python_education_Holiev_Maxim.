90f4a90502576901825c39569c256c78966247e9
moholiev-code <m.o.holiev@student.itc.khai.edu>
2026-05-25 10:25:14 +0300

    Stage 2

--- simplified_markdown_editor/simplified_markdown_editor.py
+++ simplified_markdown_editor/simplified_markdown_editor.py
@@ -22,3 +22,23 @@ while True:
 
     else:
         print("Unknown formatting type or command")
+
+markdown = ""
+
+while True:
+    command = input("Choose a formatter: ")
+
+    if command == "!help":
+        print("Available formatters: plain bold italic header link inline-code ordered-list unordered-list new-line")
+        print("Special commands: !help !done")
+
+    elif command == "!done":
+        break
+
+    elif command == "plain":
+        text = input("Text: ")
+        markdown += text
+        print(markdown)
+
+    else:
+        print("Unknown formatting type or command")
